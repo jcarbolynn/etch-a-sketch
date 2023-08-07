3bf08b5fbc67c8c7779f85b42321ef610180ff4b
jcarbolynn <36271436+jcarbolynn@users.noreply.github.com>
2023-08-07 14:01:45 -0400

    Update main.py

--- main.py
+++ main.py
@@ -7,20 +7,33 @@ screen = Screen()
 def move_forwards():
   tim.forward(10)
 
+
 def move_backwards():
   tim.backward(10)
 
-def turn_clockwise():
-  tim.right(10)
 
-def turn_counterclockwise():
-  tim.left(10)
+def turn_left():
+  new_heading = tim.heading() + 10
+  tim.setheading(new_heading)
+
+
+def turn_right():
+  new_heading = tim.heading() - 10
+  tim.setheading(new_heading)
+
+
+def clear():
+  tim.clear()
+  tim.penup()
+  tim.home()
+  tim.pendown()
 
 
 screen.listen()
-screen.onkey(key="w", fun=move_forwards)
-screen.onkey(key="s", fun=move_backwards)
-screen.onkey(key="d", fun=turn_clockwise)
-screen.onkey(key="a", fun=turn_counterclockwise)
-screen.onkey(key="space", fun=tim.clear)
+screen.onkey(move_forwards, "Up")
+screen.onkey(move_backwards, "Down")
+screen.onkey(turn_left, "Left")
+screen.onkey(turn_right, "Right")
+screen.onkey(clear, "C") #if capital c have to shift + c to use clear function
+
 screen.exitonclick()
